d8f8c6400cf1a6721c3f800ac96b8b012b2eed41
KochkinaEkaterina <gudrum1983@gmail.com>
2026-08-05 15:51:30 +0200

    add global styling and custom font integration

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript370004 App" />
   </div>
 </template>
 
@@ -18,12 +18,12 @@ export default Vue.extend({
 </script>
 
 <style>
-#app {
+/*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+}*/
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 
+import "@/assets/styles/reset.css";
+import "@/assets/styles/fonts.css";
+import "@/assets/styles/variables.css";
+import "@/assets/styles/global.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
