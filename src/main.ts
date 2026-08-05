abfdcae27c74b696a584f5db46aa5174d5b0486b
KochkinaEkaterina <gudrum1983@gmail.com>
2026-08-05 18:24:15 +0200

    refactor: restructure CSS imports, replace global styles with modular approach, and document naming conventions

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 
-import "@/assets/styles/reset.css";
-import "@/assets/styles/fonts.css";
-import "@/assets/styles/variables.css";
-import "@/assets/styles/global.css";
+import "@/assets/styles/main.css";
 
 Vue.config.productionTip = false;
 
