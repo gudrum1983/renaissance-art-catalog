9d95a472edc23e5aa219e9ae4636a9fd3e186193
KochkinaEkaterina <gudrum1983@gmail.com>
2026-08-07 13:39:11 +0200

    add `PageFooter` component and update `App.vue` layout and styles

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,21 @@
       @search="handleSearch"
       @reset-search="handleResetSearch"
     />
-
-    <ButtonBase color="accent">
-      <template v-slot:icon>
-        <IconCheck size="20" />
-      </template>
-      В корзине
-    </ButtonBase>
+    <div class="page-content">
+      <h1 class="header-page">Картины эпохи Возрождения</h1>
+      <ButtonBase color="accent">
+        <template v-slot:icon>
+          <IconCheck size="20" />
+        </template>
+        В корзине
+      </ButtonBase>
+    </div>
+    <PageFooter
+      :navigation-items="navigationItems"
+      :search-value="searchValue"
+      @search="handleSearch"
+      @reset-search="handleResetSearch"
+    />
   </div>
 </template>
 
@@ -22,6 +30,7 @@ import ButtonBase from "@/components/atoms/ButtonBase.vue";
 import IconCheck from "@/components/atoms/icons/IconCheck.vue";
 import { navigation } from "@/shared/data";
 import PageHeader from "@/components/organisms/PageHeader.vue";
+import PageFooter from "@/components/organisms/PageFooter.vue";
 
 export default Vue.extend({
   name: "App",
@@ -29,6 +38,7 @@ export default Vue.extend({
     PageHeader,
     IconCheck,
     ButtonBase,
+    PageFooter,
   },
   data() {
     return {
@@ -50,6 +60,19 @@ export default Vue.extend({
 </script>
 
 <style>
+.page-content {
+  --max-width: var(--container-max-width);
+  max-width: var(--max-width);
+  min-height: calc(100vh - 97px - 97px);
+  padding-block: 45px;
+  padding-inline: 47px;
+  margin-inline: auto;
+}
+
+.header-page {
+  font: var(--font-h1);
+  letter-spacing: var(--letter-spacing-tight);
+}
 /*#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
